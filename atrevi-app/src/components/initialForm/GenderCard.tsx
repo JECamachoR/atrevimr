a14e862f2315bb67866f1af63da41daa761a753c
javi <javierecr2811@gmail.com>
2021-09-05 11:09:49 -0500

    Translated Basics

--- atrevi-app/src/components/initialForm/GenderCard.tsx
+++ atrevi-app/src/components/initialForm/GenderCard.tsx
@@ -1,39 +1,41 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View,  Platform } from "react-native";
+import * as React from "react"
+import { StyleSheet, View,  Platform } from "react-native"
 import {
-  heightPercentageToDP as hp,
-  widthPercentageToDP as wp,
-} from "react-native-responsive-screen";
-import { RFPercentage, RFValue as rf } from "react-native-responsive-fontsize";
-import { Picker } from "@react-native-picker/picker";
+	heightPercentageToDP as hp,
+	widthPercentageToDP as wp,
+} from "react-native-responsive-screen"
+import { Picker } from "@react-native-picker/picker"
+import { t } from "i18n-js"
 
 type Props = {
   value: string | undefined,
   setValue: (v: string | undefined) => void
 }
 
-export default function GenderCard({ value, setValue }: Props) {
+export default function GenderCard({ value, setValue }: Props): React.ReactElement {
 
-  return (
-    <View style={styles.container}>
-      <Picker
-        selectedValue={value}
-        onValueChange={(itemValue) => setValue(itemValue)}
-      >
-        <Picker.Item label="Gender" value={undefined} />
-        <Picker.Item label="Male" value="Male" />
-      </Picker>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Picker
+				selectedValue={value}
+				onValueChange={(itemValue) => setValue(itemValue)}
+			>
+				<Picker.Item label={t("Gender")} value={undefined} />
+				<Picker.Item label={t("Man")} value="man" />
+				<Picker.Item label={t("Woman")} value="woman" />
+				<Picker.Item label={t("Other")} value="other" />
+			</Picker>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: wp("90%"),
-    height: hp("8%"),
-    justifyContent: "center",
-    borderBottomWidth: 2,
-    borderBottomColor: "#e5e5e5",
-    overflow:Platform.OS=="ios"? "hidden":"visible",
-  },
-});
+	container: {
+		width: wp("90%"),
+		height: hp("8%"),
+		justifyContent: "center",
+		borderBottomWidth: 2,
+		borderBottomColor: "#e5e5e5",
+		overflow:Platform.OS=="ios"? "hidden":"visible",
+	},
+})
--- atrevi-app/src/components/initialForm/OccupationCard.tsx
+++ atrevi-app/src/components/initialForm/OccupationCard.tsx
@@ -1,39 +1,40 @@
-import React, { useState } from "react";
-import { StyleSheet, View,Platform } from "react-native";
+import * as React from "react"
+import { StyleSheet, View,Platform } from "react-native"
 import {
-  heightPercentageToDP as hp,
-  widthPercentageToDP as wp,
-} from "react-native-responsive-screen";
-import { RFPercentage, RFValue as rf } from "react-native-responsive-fontsize";
-import { Picker } from "@react-native-picker/picker";
+	heightPercentageToDP as hp,
+	widthPercentageToDP as wp,
+} from "react-native-responsive-screen"
+import { Picker } from "@react-native-picker/picker"
+import { t } from "i18n-js"
 
 type Props = {
   value: string | undefined,
   setValue: (v: string | undefined) => void
 }
 
-export default function OccupationCard({ value, setValue }: Props) {
+export default function OccupationCard({ value, setValue }: Props): React.ReactElement {
 
-  return (
-    <View style={styles.container}>
-      <Picker
-        selectedValue={value}
-        onValueChange={(itemValue) => setValue(itemValue)}
-      >
-        <Picker.Item label="Occupation" value={undefined} />
-        <Picker.Item label="Programmer" value="Programmer" />
-      </Picker>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Picker
+				selectedValue={value}
+				onValueChange={(itemValue) => setValue(itemValue)}
+				mode="dialog"
+			>
+				<Picker.Item label={t("Occupation")} value={undefined} />
+				<Picker.Item label={t("Student")} value="student" />
+			</Picker>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: wp("90%"),
-    height: hp("8%"),
-    justifyContent: "center",
-    borderBottomWidth: 2,
-    borderBottomColor: "#e5e5e5",
-    overflow:Platform.OS=="ios"? "hidden":"visible",
-  },
-});
+	container: {
+		width: wp("90%"),
+		height: hp("8%"),
+		justifyContent: "center",
+		borderBottomWidth: 2,
+		borderBottomColor: "#e5e5e5",
+		overflow:Platform.OS=="ios"? "hidden":"visible",
+	},
+})
