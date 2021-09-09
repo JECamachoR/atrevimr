702b1603f86fab7374e2c2414aaad10a4933a01d
javi <javierecr2811@gmail.com>
2021-09-09 14:46:26 -0500

    updated metro config

--- atrevi-app/metro.config.js
+++ atrevi-app/metro.config.js
@@ -1,13 +1,29 @@
-module.exports = {
-  resolver: {
-    blacklistRE: /#current-cloud-backend[\\/].*/
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getDefaultConfig } = require("@expo/metro-config")
+
+const defaultConfig = getDefaultConfig(__dirname)
+
+defaultConfig.resolver.blacklistRE = /#current-cloud-backend[\\/].*/
+defaultConfig.transformer = {
+	getTransformOptions: async () => ({
+		transform: {
+			experimentalImportSupport: false,
+			inlineRequires: false,
+		},
+	}),
+},
+
+module.exports = defaultConfig
+// module.exports = {
+//   resolver: {
+//     blacklistRE: /#current-cloud-backend[\\/].*/
+//   },
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: false,
+//       },
+//     }),
+//   },
+// };
